3eea6feb55b881f9ca10b0420ccb328dc6e05198
jreilly7 <jreilly7@nd.edu>
2016-12-09 17:47:48 -0500

    add name and user reference to item

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209214456) do
+ActiveRecord::Schema.define(version: 20161209224519) do
 
   create_table "items", force: :cascade do |t|
     t.text     "item_description"
     t.text     "link"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "name"
+    t.integer  "user_id"
   end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "memberships", force: :cascade do |t|
     t.integer  "swap_id"
