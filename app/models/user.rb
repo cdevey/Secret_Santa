ac2d1133d97f2d4c9ca08dab60317dc12abc5630
Jonathan Arp <jarp@nd.edu>
2016-12-12 09:03:19 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-#  def to_s
-#    user
-#  end
+  def to_s
+    # you need to call an attribute or attributes. Calling 'user' 
+    "#{first_name} #{last_name}"
+  end
 
 
 validates :first_name, :last_name, :email, :password, :shipping_address, :bio, presence: true
