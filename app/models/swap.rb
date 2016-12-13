a915a9f4a5714c23068755598ec09373f1a90f2a
jreilly7 <jreilly7@nd.edu>
2016-12-12 19:18:47 -0500

    minor change

--- app/models/swap.rb
+++ app/models/swap.rb
@@ -3,12 +3,19 @@ class Swap < ActiveRecord::Base
 has_many :memberships
 has_many :users, through: :memberships
 
+validates :location, :when, :swap_description, presence: true
+
+
   def to_s
     "#{swap_description}"
   end
 
-  validates :location, :when, :swap_description, presence: true
-  
+
+
+
+
+
+
 
 
 end
